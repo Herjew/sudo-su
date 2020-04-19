9bfefd953d681f395fe7051900388d3573078e74
wangxian@rockfintech.com <wangxian@rockfintech.com>
2020-04-19 23:34:06 +0800

    适配

--- src/Controllers/SudoSuController.php
+++ src/Controllers/SudoSuController.php
@@ -2,6 +2,8 @@
 
 namespace VIACreative\SudoSu\Controllers;
 
+use App\Models\BackendAdmin;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use VIACreative\SudoSu\SudoSu;
 use Illuminate\Routing\Controller;
@@ -17,7 +19,8 @@ class SudoSuController extends Controller
 
     public function loginAsUser(Request $request)
     {
-        $this->sudoSu->loginAsUser($request->userId, $request->originalUserId);
+        $admin = $this->sudoSu->loginAsUser($request->userId, $request->originalUserId);
+        $this->authenticated($request, $admin);
 
         return redirect()->back();
     }
@@ -28,4 +31,21 @@ class SudoSuController extends Controller
 
         return redirect()->back();
     }
+
+    /**
+     * 3 1 The user has been authenticated.
+     * @param  \Illuminate\Http\Request  $request
+     * @param  BackendAdmin  $admin
+     * @return mixed
+     */
+    protected function authenticated(Request $request, $admin)
+    {
+        $admin->setAttribute('last_login_at', Carbon::now()->toDateTimeString());
+        $admin->setAttribute('last_login_ip', $request->getClientIp());
+        $admin->update();
+
+        $admin->lastSession();
+    }
+
+
 }
